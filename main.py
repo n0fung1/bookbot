664217cd9e6a97256a2779b3729300060879c330
n0fung1 <joe@holliday.tech>
2024-08-04 12:26:57 -0400

    added letter count, sort on, and 'finished' main

--- main.py
+++ main.py
@@ -9,18 +9,38 @@ def open_file(file_to_open):
 def count_letters(file_contents):
     words = file_contents.split()
     letter_count = {}
+    lower_words = [] 
     for word in words:
-        #
-    print(letter_count)
+        lower_words.append(word.lower())
+    for word in lower_words:
+        for letter in word:
+            if letter in letter_count:
+                letter_count[letter] = letter_count[letter] + 1
+            else:
+                letter_count[letter] = 1
+    return letter_count
 
+def convert_to_list_dict(dictionary):
+    list_of_dicts = []
+    for k in dictionary:
+        if k.isalpha():
+            list_of_dicts.append({"character":k, "num":dictionary[k]})
+    return list_of_dicts
+
+def sort_on(dict):
+    return dict["num"]
 
 def main():
     file_to_open = "books/frankenstein.txt"
     file_contents = open_file(file_to_open)
     word_count = count_words(file_contents)
-    print(f"{word_count} words in {file_to_open}")
-    count_letters(file_contents)
-
-
-
+    character_list_dict = count_letters(file_contents)
+    print(f"--- Begin report of {file_to_open} ---")
+    print(f"{word_count} words in the document\n")
+    final_list = convert_to_list_dict(character_list_dict)
+    final_list.sort(reverse=True, key=sort_on)
+    for d in final_list:
+        print(f"The {d["character"]} character was found {d["num"]} times.")
+    print("\n--- End report ---")
+    #print(character_list_dict['p'])
 main()
