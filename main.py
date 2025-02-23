fbbef1f06c14855b2178028a86590c966a5e4044
n0fung1 <n0fung1@proton.me>
2025-02-22 22:45:18 -0500

    add stats.py with word_count()

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from stats import word_count
+
 def get_book_text(book_path):
     with open(book_path) as book:
         contents = book.read()
         return str(contents)
 
+    
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
-
+    num_words = word_count(get_book_text("./books/frankenstein.txt"))
+    print(f"{num_words} words found in the document.")
+    
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+
+def word_count(text):
+    text_list = text.split()
+    return len(text_list)
