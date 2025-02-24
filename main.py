d33c0ec7245d4e5e9aed24291865d7dfca1d169a
n0fung1 <n0fung1@proton.me>
2025-02-23 20:35:06 -0500

    add __pycache__ to gitignore, char_count() to stats and import to main.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, char_count
 
 def get_book_text(book_path):
     with open(book_path) as book:
@@ -9,5 +9,7 @@ def get_book_text(book_path):
 def main():
     num_words = word_count(get_book_text("./books/frankenstein.txt"))
     print(f"{num_words} words found in the document.")
+
+    print(char_count(get_book_text("./books/frankenstein.txt")))
     
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,12 @@
-
 def word_count(text):
     text_list = text.split()
     return len(text_list)
+
+def char_count(text):
+    char_count_dict = {}
+    for char in text:
+        if char.lower() in char_count_dict:
+            char_count_dict[char.lower()] += 1
+        else:
+            char_count_dict[char.lower()] = 1
+    return char_count_dict
